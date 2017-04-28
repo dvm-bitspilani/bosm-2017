734a75ebbe304bfa2669c7c7ff94e4a5114e647e
Megh-Thakkar <megh.1211@gmail.com>
2017-04-29 00:55:27 +0530

    Added loader, 16 link

--- Subscribe/urls.py
+++ Subscribe/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from Subscribe import views
+from django.views.generic import RedirectView
 
 app_name = 'Subscribe'
 
 urlpatterns = [
     #url(r'^subscribe$', views.subscribe, name='subscribe'),
+    url(r'', RedirectView.as_view(url = '^$'))
     url(r'^$', views.index, name='index'),
-    url(r'^2017/download$', views.get_list, name='prc list'),
+    url(r'^2017/download$', views.get_list, name='pcr list'),
 ]
