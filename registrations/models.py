47ca9c3accc95553fd0af64b4112763f08265de5
Megh-Thakkar <megh.1211@gmail.com>
2017-08-03 01:07:47 +0530

    updated models

--- registrations/models.py
+++ registrations/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from events.models import *
 from regsoft.models import *
 
-class TeamCaptain(models.Model):
+class GroupLeader(models.Model):
 
 	GENDERS = (
 
@@ -19,15 +19,34 @@ class TeamCaptain(models.Model):
 	state = models.CharField(max_length=50)
 	phone = models.BigIntegerField()
 	email = models.EmailField(unique=True)
-	email_verified = models.BooleanField(default=False)
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	event = models.ForeignKey(Event, on_delete=models.CASCADE)
-	controlz_paid = models.BooleanField(default=False)
-	coach = models.CharField(max_length=200)
+	gender = models.CharField(max_length=10, choices=GENDERS)
 	
 	def __unicode__(self):
 
 		return self.name + ' ' + self.college
+
+	class Meta:
+
+		verbose_name_plural = 'Group Leaders'
+
+
+class TeamCaptain(models.Model):
+
+	name = models.CharField(max_length=200)
+	email = models.EmailField(blank=True, null=True)
+	email_verified = models.BooleanField(default=False)
+	event = models.ForeignKey(Event, on_delete=models.CASCADE)
+	g_l = models.ForeignKey(GroupLeader, on_delete=models.CASCADE, default=None)
+	firewallz_passed = models.NullBooleanField('passed firewallz_o', null=True, blank=True)
+	acco = models.NullBooleanField('passed recnacc', null=True, blank=True)
+	room = models.ForeignKey(Room, null=True, blank=True)
+	controlz_paid = models.BooleanField(default=False)
+	is_single = models.NullBooleanField()
+
+	def __unicode__(self):
+
+		return self.name + '-' + str(self.g_l.college)
 
 	class Meta:
 
@@ -37,6 +56,7 @@ class Participant(models.Model):
 
 	name = models.CharField(max_length=200)
 	captain = models.ForeignKey(TeamCaptain, on_delete=models.CASCADE)
+	firewallz_passed = models.NullBooleanField('passed firewallz_o', null=True, blank=True)
 	acco = models.NullBooleanField('passed recnacc', null=True, blank=True)
 	room = models.ForeignKey(Room, null=True, blank=True)
 
