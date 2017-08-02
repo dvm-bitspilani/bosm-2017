22e1694b8975a655ff88d8e9c2872f7abf11618f
Megh-Thakkar <megh.1211@gmail.com>
2017-08-02 17:17:06 +0530

    Base models made

--- registrations/models.py
+++ registrations/models.py
@@ -14,7 +14,7 @@ class TeamCaptain(models.Model):
 		)
 	
 	name = models.CharField(max_length=200)
-	college = models.CharField(max_length=200)
+	college = models.CharField(max_length=100)
 	city = models.CharField(max_length=100)
 	state = models.CharField(max_length=50)
 	phone = models.BigIntegerField()
