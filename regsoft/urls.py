c1b74180a8067f1ddbf5d7ce00150f6dbcb249b7
tushar goel <tushargoel_1999@yahoo.com>
2017-08-30 21:38:54 +0530

    recnacc

--- regsoft/urls.py
+++ regsoft/urls.py
@@ -12,8 +12,12 @@ urlpatterns = [
 
 	########## RecNAcc #############
 	url(r'^recnacc/$', views.recnacc_home, name='recnacc-home'),
-	url(r'^recnacc/allot_room/(?P<gl_id>\d+)/$', views.allot_room, name='allot_room'),
-	url(r'recnacc/main_list',views.main_list, name="main_list"),
+	url(r'^recnacc/college/(?P<gl_id>\d+)/$', views.recnacc_college, name='recnacc-college'),
+	url(r'^recnacc/team/(?P<tc_id>\d+)/$', views.recnacc_team, name='recnacc-team'),
+	url(r'^recnacc/change/$',views.recnacc_change, name='change'),
+	url(r'^recnacc/college_vs_bhavan/$', views.college_vs_bhavan, name='college_vs_bhavan'),
+	url(r'^recnacc/all_bhavans/$', views.all_bhavans, name='all_bhavans'),
+	url(r'^recnacc/firewallz_approved/$', views.firewallz_approved, name='firewallz_approved'),
 	
 	########## Controlz #############
 	url(r'^controlz/$', views.controlz_home, name='controlz-home'),
